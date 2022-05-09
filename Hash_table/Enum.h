00f31e636c053cbec5a50d4d30457ab81d9c7889
Sibonji <bulatsingat4@gmail.com>
2022-05-09 18:26:37 +0200

    Enums moved into special .h file

---
+++ Hash_table/Enum.h
@@ -0,0 +1,23 @@
+#ifndef ENUM_H
+#define ENUM_H
+
+enum ret_names{
+    success,
+    found,
+
+    zero_login,
+    zero_pointer,
+    bad_size,
+    elem_exist,
+    bad_alloc,
+    not_exist,
+
+    undef_cmd_name,
+
+    valid,
+    not_valid,
+    test_fail,
+    test_pass
+};
+
+#endif
