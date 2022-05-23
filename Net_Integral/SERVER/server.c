4136d96f9874793ed5abaa34fcd9d3f7748167f1
Sibonji <bulatsingat4@gmail.com>
2022-05-23 18:54:05 +0200

    Removed loop mode

--- Net_Integral/SERVER/server.c
+++ Net_Integral/SERVER/server.c
@@ -2,22 +2,6 @@
 #include "../LIB/enum.h"
 
 int main (int argc, char* argv[]) {
-    if (argc == 2 && strcmp(argv[1], "loop_mode") == 0) {
-        while (1) {
-            printf ("Enter pc quantity, if you want to quit enter -1: ");
-            fflush (stdout);
-
-            int pc_quant = 0;
-            scanf ("%d", &pc_quant);
-            if (pc_quant > 0)
-                server_start (pc_quant);
-            else if (pc_quant == -1)
-                return 0;
-            else
-                printf ("Incorrect pc quantity!\n");
-        }
-    }
-
     int pc_quant = check_args (argc, argv);
     if (pc_quant < 0)
         fprintf (stderr, "Incorrect command line arguments!\n");
